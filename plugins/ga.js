893a1a3a3d5b3d1a97957bf503e2a0e2186400bc
Clark Mitchell <ClarkMitchell@users.noreply.github.com>
2018-09-04 09:12:41 -0600

    Update ga.js

--- plugins/ga.js
+++ plugins/ga.js
@@ -10,6 +10,11 @@ export default ({ app }) => {
   /*
   ** Include Google Optimize Script
   */
+  <style>
+  .async-hide {
+    opacity: 0 !important
+  }
+  </style>
   (function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
   h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
   (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
