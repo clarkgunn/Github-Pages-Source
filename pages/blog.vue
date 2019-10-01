ca96bf1f2397bd0914546568abd1319862c2a56c
Clark Mitchell <ClarkMitchell@users.noreply.github.com>
2019-10-01 09:14:08 -0400

    Create blog.vue

---
+++ pages/blog.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hero-body">
+
+  </div>
+</template>
+
+<script>
+export default {
+  created: function () {
+      location.href = 'https://clarkmitchell.dev/blog/'
+    }
+}
+</script>
